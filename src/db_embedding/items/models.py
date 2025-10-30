a72359a6de381ec567e102863afbb2bc0af447d9
LeXXai <lexxai@gmail.com>
2025-10-30 03:24:53 +0200

    add migrations

--- src/db_embedding/items/models.py
+++ src/db_embedding/items/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
+from pgvector.django import VectorField
+from django.contrib.postgres.search import SearchVectorField
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Optional: full-text search vector
+    search_vector = SearchVectorField(null=True)
+
+
+class ItemEmbedding(models.Model):
+    item = models.OneToOneField(
+        Item, on_delete=models.CASCADE, related_name="embedding"
+    )
+    vector = VectorField(
+        dimensions=settings.VECTOR_EMBEDDIG_DIMENSIONS
+    )  # OpenAI embedding vector size: 1536
+    model = models.CharField(max_length=50, default=settings.EMBEDDIG_MODEL_NAME)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class QueryEmbedding(models.Model):
+    query_hash = models.CharField(max_length=64, unique=True)  # SHA256
+    vector = VectorField(dimensions=1536)
+    created_at = models.DateTimeField(auto_now_add=True)
