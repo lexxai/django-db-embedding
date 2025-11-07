d819e87d3df7455c8357053a8d972eea4fa510ea
LeXXai <lexxai@gmail.com>
2025-11-07 18:56:57 +0200

    OllamaEmbeddingBackend

--- src/embeddings/ollama_backend.py
+++ src/embeddings/ollama_backend.py
@@ -10,12 +10,16 @@ logger = logging.getLogger(__name__)
 class OllamaEmbeddingBackend(OpenAIEmbeddingBackend):
     name = "ollama"
 
-    def __init__(self, model: str = None, dimensions: int = None, api_key: str = None, base_url: str = None, **kwargs):
+    def __init__(
+        self,
+        model: str = None,
+        dimensions: int = None,
+        preload: bool = False,
+        api_key: str = None,
+        base_url: str = None,
+        **kwargs,
+    ):
         model = model or settings.OLLAMA_EMBEDDIG_MODEL_NAME
-        super().__init__(model, dimensions)
-        assert self.model, "OLLAMA_EMBEDDIG_MODEL_NAME must be set"
-        self.api_key = api_key or settings.OLLAMA_API_KEY
-        assert self.api_key, "OLLAMA_API_KEY must be set"
-        self.base_url = base_url or settings.OLLAMA_BASE_URL
-        assert self.base_url, "OLLAMA_API_BASE must be set"
-        self.kwargs = kwargs
+        api_key = api_key or settings.OLLAMA_API_KEY
+        base_url = base_url or settings.OLLAMA_BASE_URL
+        super().__init__(model, dimensions, preload=preload, api_key=api_key, base_url=base_url, **kwargs)
